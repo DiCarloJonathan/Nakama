e6209e02f6c35327aa7d65174d70794fb07bf543
mugicaj <mugicaj@oregonstate.edu>
2016-02-09 14:56:33 -0800

    Update forgot_pasword_go.php

--- forgot_pasword_go.php
+++ forgot_pasword_go.php
@@ -4,9 +4,10 @@
 include ("email.php");
 if (isset($_POST["username"])){
 	$username = $_POST["username"];
-	if($result = $mysqli->query("select email from Users where userName == $usename")){//////////ned to fix
+	if($result = $mysqli->query("select email from Users where userName == $username")){//////////ned to fix
 		$body = "click this link to reset your password: "."put my sql var here";///////////need to send email that takes you to web page ask teacher how to keep well.
 		send_email("$email","Reset Your password", "$body","");
+		
 		echo "Email has been sent this may take a while";
 	}
 	else 
